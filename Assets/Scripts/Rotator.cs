89393e1f3d43201395e96154e3803da69e1088d6
1264109842 <40214255+1264109842@users.noreply.github.com>
2018-08-07 19:18:27 -0400

    zz

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate((new Vector3(100, 150, 200)) * Time.deltaTime);
+	}
+}
